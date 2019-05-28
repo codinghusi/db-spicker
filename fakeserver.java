d2dfdb519ffc4cfcbe2f24a25ad0bd7637e5ebb2
Gerrit Weiermann <gerrit.weiermann@gmx.de>
2019-05-28 19:04:10 +0200

    db for future

--- fakeserver.java
+++ fakeserver.java
@@ -5,29 +5,33 @@
 public class SpielEndpoint {
 	
 	private List<Session> spieler = new ArrayList<>();
-	public boolean gestarted = false;
+	public boolean gestartet = false;
 	
 	@OnMessage
 	public String onMessage(String nachricht) {
 		// Lehrer sendet start
-		if (nachricht.equals("lehrer: start")) {
+		if (nachricht.equals("befehl: start")) {
 			sendToAll("Jetzt gehts los!");
-			gestarted = true;
+			gestartet = true;
 		}
 		// Lehrer sendet stop
-		else if (nachricht.equals("lehrer: stop")) {
+		else if (nachricht.equals("befehl: stop")) {
 			sendToAll("Und aus die Maus!");
-			gestarted = false;
+			gestartet = false;
 		}
 		// Sende Nachrichten von Lehrern, hat immer sende Berechtigung
-		else if (nachricht.startingWith("lehrer: ")){
+		else if (nachricht.startsWith("lehrer: ")){
 			sendToAll(nachricht);
 		}
 		// Sende Nachrichten von Schülern nur wenn es gestartet ist
-		else if (nachricht.startingWith("schueler: ")){
-			if (gestarted) {
+		else if (nachricht.startsWith("schueler: ")){
+			if (gestartet) {
 				sendToAll(nachricht);
 			}
+		}
+		// Wenn was ganz anderes gekommen ist
+		else {
+			System.out.println("Die Nachricht '" + nachricht + "' ist nicht im richtigen Format");
 		}
 		return null;
 	}
@@ -42,10 +46,14 @@ public class SpielEndpoint {
 		spieler.remove(session);
 	}
 	
-	public void sendToAll(String message) {
+	public void sendToAll(String nachricht) {
 		for (int i = 0; i < spieler.size(); ++i) {
 			Session session = spieler.get(i);
-			session.getBasicRemote().sendText(message);
+			try {
+				session.getBasicRemote().sendText(nachricht);
+			} catch(IOException ioe) {
+				ioe.printStackTrace();
+			}
 		}
 	}
 }
--- lehrer.js
+++ lehrer.js
@@ -1,6 +1,10 @@
 
 var websocket = new WebSocket("ws://localhost:8080/spielendpoint");
 
+websocket.onmessage = function(nachricht) {
+	ausgeben(nachricht);
+};
+
 function anschalten() {
 	websocket.send("lehrer: start");
 	alert("angeschaltet");
@@ -15,4 +19,10 @@ function senden() {
 	var eingabefeld = document.getElementById("eingabefeld");
 	var nachricht = eingabefeld.value;
 	websocket.send("lehrer: " + nachricht);
+}
+
+function befehlen() {
+	var eingabefeld = document.getElementById("eingabefeld");
+	var nachricht = eingabefeld.value;
+	websocket.send("befehl: " + nachricht);
 }
